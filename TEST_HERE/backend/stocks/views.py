4c0c6067fc5fb1bf36f46f09ef780f191a193095
IM HYUN JOO <ihjkong@naver.com>
2024-11-20 10:44:42 +0900

    게임 시작 날짜 랜덤 설정

--- TEST_HERE/backend/stocks/views.py
+++ TEST_HERE/backend/stocks/views.py
@@ -2,10 +2,27 @@ from django.http import JsonResponse
 from .models import Stock
 import FinanceDataReader as fdr
 from datetime import datetime, timedelta
+import random ######
 
 def stock_data(request, ticker):
     try:
-        start_date = datetime.strptime('2024-09-16', '%Y-%m-%d')
+
+        # 랜덤 날짜 구현 코드
+        # - 다만 돌아갈때마다 rand값이 바뀌므로 "게임 start할때" 한번만 받아야함
+        # game_start 변수(true/false) 만들어거 구현하기
+        start_year = random.randrange(2020,2024)    # 2020~2023 까지 랜덤
+        start_month = random.randrange(1,13)      # 1~12 까지 랜덤
+        if start_month in [1,3,5,7,8,10,12] :
+            start_date = random.randrange(1,32)      # 1~31 까지 랜덤
+        elif star_month in [4,6,9,11] :
+            start_date = random.randrange(1,31)      # 1~30 까지 랜덤
+        else : # 2월
+            start_date = random.randrange(1,29)      # 1~28 까지 랜덤
+    
+        temp = str(start_year)+'-'+str(start_month)+'-'+str(start_date)
+        start_date = datetime.strptime(temp, '%Y-%m-%d')
+        
+        # start_date = datetime.strptime('2024-09-16', '%Y-%m-%d') # 기존 코드
         end_date = start_date
 
         data = []
